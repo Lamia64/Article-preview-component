639fea2b3276626fb3ce608cf3d7c11f5a3051ed
Lamia64 <109281142+Lamia64@users.noreply.github.com>
2023-09-02 17:48:15 +0300

    Add files via upload
    commint

---
+++ script.js
@@ -0,0 +1,34 @@
+
+var tooltip = document.getElementById('tooltip');
+
+var shareOption = document.querySelector('.share-option');
+document.querySelector('.share-button').addEventListener('click' , function () {
+
+    shareOption.classList.toggle('active');
+})
+
+
+
+
+//****another way to show the tooltip  by adding onclick event on the share button****//
+
+// var i ;
+// function show() {
+     
+//     if (i === 1) {
+//       tooltip.style.visibility = 'hidden';
+//         i = 0 ; 
+//     }
+//     else{
+//         tooltip.style.visibility = 'visible';
+//         i = 1;
+//     } 
+// }
+
+   
+  
+    
+
+
+
+
